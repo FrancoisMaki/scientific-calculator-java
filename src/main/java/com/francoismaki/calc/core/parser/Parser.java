7af5d9dcfcfe1e7449b03a5df830d2698be02cce
FrancoisMaki <francoismakijess@gmail.com>
2026-03-23 16:28:17 +0100

    add Shunting Yard Algorithm in Parser class

--- src/main/java/com/francoismaki/calc/core/parser/Parser.java
+++ src/main/java/com/francoismaki/calc/core/parser/Parser.java
@@ -1,6 +1,6 @@
 package com.francoismaki.calc.core.parser;
 
-import java.util.Map;
+import java.util.*;
 
 public class Parser {
     private static final Map<String, Integer> PRECEDENCE = Map.of(
@@ -17,6 +17,49 @@ public class Parser {
 
     private boolean isRightAssociative(String operator){
         return operator.equals("^");
+    }
+
+    //  Shunting-Yard Algorithm
+
+    public List<Token> parseToRPN(List<Token> tokens){
+        List<Token> outputQueue = new ArrayList<>();
+        Deque<Token> operatorStack =  new ArrayDeque<>();
+
+        for (Token token : tokens){
+            switch (token.getType()){
+                case NUMBER:
+                    outputQueue.add(token);
+                    break;
+
+                case FUNCTION:
+                    outputQueue.add(token);
+                    break;
+
+                case OPERATOR:
+                    while (!operatorStack.isEmpty() &&
+                            (operatorStack.peek().getType() == TokenType.OPERATOR ||
+                                    operatorStack.peek().getType() == TokenType.FUNCTION)){
+
+                        Token top = operatorStack.peek();
+
+                        if (top.getType() == TokenType.FUNCTION ||
+                            getPrecedence(top.getValue()) > getPrecedence(token.getValue()) ||
+                                (getPrecedence(top.getValue()) == getPrecedence(token.getValue()) &&
+                                        !isRightAssociative(token.getValue()))){
+                            outputQueue.add(operatorStack.pop());
+                        }else {
+                            break;
+                        }
+                    }
+
+                    operatorStack.push(token);
+                    break;
+
+                case PAREN_LEFT:
+            }
+        }
+
+        return outputQueue;
     }
 }
 
