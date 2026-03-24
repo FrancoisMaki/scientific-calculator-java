ab26257effe7385d45547dad259749f88a69d4fb
FrancoisMaki <francoismakijess@gmail.com>
2026-03-24 14:54:24 +0100

    Add AST Builder in Parse.java to differentiate between Operators and Functions

--- src/main/java/com/francoismaki/calc/Main.java
+++ src/main/java/com/francoismaki/calc/Main.java
@@ -4,6 +4,7 @@ import com.francoismaki.calc.core.ast.Constant;
 import com.francoismaki.calc.core.ast.Expression;
 import com.francoismaki.calc.core.operations.binary.Addition;
 import com.francoismaki.calc.core.operations.binary.Multiplication;
+import com.francoismaki.calc.core.parser.Parser;
 import com.francoismaki.calc.core.parser.Token;
 import com.francoismaki.calc.core.parser.Tokenizer;
 
@@ -11,14 +12,21 @@ import java.util.List;
 
 public class Main {
     static void main() {
-        String ecuacion = "35.5 + sin(90) * (2^4)";
-        System.out.println("Generando la respuesta " + ecuacion);
+        String ecuacion = "((3 * 1 + 5) * (4 + 2)) + 2";
+        //String ecuacion = "(3 * 1 + 5 * 4 + 2) + 2";
+        System.out.println("Ecuación Original: " + ecuacion);
+        //  ((3 * 1 + 5) * (4 + 2)) + 2   -->     3 1 * 5 + 4 2 + * 2 +
+        //  (3 * 1 + 5 * 4 + 2) + 2       -->     3 1 * 5 4 * + 2 + 2 +
 
         Tokenizer tokenizer = new Tokenizer(ecuacion);
         List<Token> tokens = tokenizer.tokenize();
 
-        for (Token token : tokens){
-            System.out.println(token);
+        Parser parser = new Parser();
+        List<Token> rpnTokens = parser.parseToRPN(tokens);
+
+        System.out.println("Orden RPN:");
+        for (Token token : rpnTokens) {
+            System.out.print(token.getValue() + " ");
         }
     }
 }
--- src/main/java/com/francoismaki/calc/core/parser/Parser.java
+++ src/main/java/com/francoismaki/calc/core/parser/Parser.java
@@ -1,5 +1,10 @@
 package com.francoismaki.calc.core.parser;
 
+import com.francoismaki.calc.core.ast.Constant;
+import com.francoismaki.calc.core.ast.Expression;
+import com.francoismaki.calc.core.operations.binary.*;
+import com.francoismaki.calc.core.operations.unary.*;
+
 import java.util.*;
 
 public class Parser {
@@ -56,10 +61,79 @@ public class Parser {
                     break;
 
                 case PAREN_LEFT:
+                    operatorStack.push(token);
+                    break;
+
+                case PAREN_RIGHT:
+                    while (!operatorStack.isEmpty() && operatorStack.peek().getType() != TokenType.PAREN_LEFT){
+                        outputQueue.add(operatorStack.pop());
+                    }
+                    if (!operatorStack.isEmpty() && operatorStack.peek().getType() == TokenType.PAREN_LEFT){
+                        operatorStack.pop();
+                    }
+                    if (!operatorStack.isEmpty() && operatorStack.peek().getType() == TokenType.FUNCTION){
+                        outputQueue.add(operatorStack.pop());
+                    }
             }
         }
 
+        while (!operatorStack.isEmpty()){
+            Token top = operatorStack.pop();
+            if (top.getType() == TokenType.PAREN_LEFT || top.getType() == TokenType.PAREN_RIGHT){
+                throw new IllegalArgumentException("Error de sintaxis, parentesis incompletos");
+            }
+            outputQueue.add(top);
+        }
+
         return outputQueue;
+    }
+
+    public Expression buildAST(List<Token> rpnTokens){
+        Deque<Expression> stack = new ArrayDeque<>();
+
+        for (Token token : rpnTokens){
+            switch (token.getType()){
+                case NUMBER:
+                    stack.push(new Constant(Double.parseDouble(token.getValue())));
+                    break;
+
+                case OPERATOR:
+                    Expression right = stack.pop();
+                    Expression left = stack.pop();
+
+                    stack.push(createBinaryOperation(token.getValue(), left, right));
+                    break;
+
+                case FUNCTION:
+                    Expression operand = stack.pop();
+                    stack.push(createUnaryOperation(token.getValue(), operand));
+                    break;
+            }
+        }
+        return stack.pop();
+    }
+
+    private Expression createBinaryOperation(String op, Expression left, Expression right){
+        return switch (op){
+            case "+"    ->  new Addition(left, right);
+            case "-"    ->  new Substraction(left, right);
+            case "*"    ->  new Multiplication(left, right);
+            case "/"    ->  new Division(left, right);
+            case "^"    ->  new Power(left, right);
+            default     ->  throw new IllegalArgumentException("Operador desconocido: " + op);
+        };
+    }
+
+    private Expression createUnaryOperation(String func, Expression operand){
+        return switch (func){
+            case "sin"          ->  new Sin(operand);
+            case "cos"          ->  new Cos(operand);
+            case "tan"          ->  new Tan(operand);
+            case "ln"           ->  new Ln(operand);
+            case "log10"        ->  new Log10(operand);
+            case "squareRoot"   ->  new SquareRoot(operand);
+            default -> throw new IllegalArgumentException("Funcion desconocida " + func);
+        };
     }
 }
 
